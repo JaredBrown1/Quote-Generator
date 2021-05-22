485e115f34ca5ad112a9feb3fc77fa7b42497f92
Jared Brown <jaredbrown1@hotmail.com>
2021-05-22 02:53:22 -0700

    Loader implemented

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const quoteContainer = document.getElementById("quote-generator");
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+	loading();
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 	// Check if author field is blank and replace it with 'unknown'
@@ -22,11 +36,14 @@ function newQuote() {
 	} else {
 		quoteText.classList.remove("long-quote");
 	}
+	// Set Quote, Hide Loader
 	quoteText.textContent = quote.text;
+	complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+	loading();
 	const apiUrl = "https://type.fit/api/quotes";
 	try {
 		const response = await fetch(apiUrl);
